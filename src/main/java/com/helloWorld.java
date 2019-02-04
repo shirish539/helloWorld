cf374bb8e154cf2529d1a2c540c025a2d6a6bd55
shirish539 <shirish.borle@gmail.com>
2019-02-04 15:38:33 +0530

    fist copy

---
+++ src/main/java/com/helloWorld.java
@@ -0,0 +1,7 @@
+package com;
+
+public class helloWorld {
+        public  void  printName (){
+
+        }
+}
---
+++ src/test/java/com/helloWorldTest.java
@@ -0,0 +1,25 @@
+package com;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class helloWorldTest {
+
+    @Test
+    public  void printName(){
+
+    }
+    @Test
+    public  void printName1(){
+
+    }
+
+    @Test
+    public  void printName2(){
+
+    }
+
+
+
+}
